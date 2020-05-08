32c54422f075d001ff0eb3006333419aa1074c95
dolinskaya <dolinskaya@tutu.tech>
2020-05-08 16:50:52 +0300

    fixup

--- node_modules/nightwatch-html-reporter/lib/relativePath.js
+++
@@ -1,7 +0,0 @@
-var path = require('path');
-
-module.exports = function(outputFilename, screenshotPath) {
-  var absOut = path.resolve(outputFilename);
-  var absDir = path.dirname(absOut);
-  return path.relative(absDir, screenshotPath);
-}
